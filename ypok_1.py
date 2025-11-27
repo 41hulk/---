63c219806a4513858546e60cb69fc76a9137d8e3
Ntare Guy <gntare2@gmail.com>
2025-11-27 19:01:49 +0300

    exe2

--- ypok_1.py
+++ ypok_1.py
@@ -1,1 +1,28 @@
-print('Hello')
+"""
+Задание 2
+Напишите программу, которая определяет, состоит ли
+двузначное число, введённое с клавиатуры, из одинаковых цифр. Если
+состоит, то программа выводит «ДА», в противном случае программа выводит
+«НЕТ».
+
+"""
+    
+a=0
+b=0
+
+print("Enter the first Number")
+
+a = input()
+
+print("The first number is ",a)
+
+print("Enter the second number")
+b = input()
+
+print("The second number is ",b)
+
+if a==b:
+    print("ДА")
+else:
+    print("НЕТ")
+    
